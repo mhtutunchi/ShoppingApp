eb65bd48399e45d517dfb6681e37ff306b5a1b2f
mohammadhosseintutunchi <75937774+mohammadhosseintutunchi@users.noreply.github.com>
2021-08-04 10:31:54 +0430

    Shopping App

--- app/src/main/java/com/example/shoppingapp/fragment/HomeFragment.java
+++ app/src/main/java/com/example/shoppingapp/fragment/HomeFragment.java
@@ -14,12 +14,13 @@ public class HomeFragment extends Fragment {
 
     public HomeFragment() {
     }
-
+View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 }
  
---
+++ app/src/main/java/com/example/shoppingapp/model/Banner.java
@@ -0,0 +1,25 @@
+package com.example.shoppingapp.model;
+
+public class Banner {
+
+    private String id ,  link_img;
+
+    public Banner() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLink_img() {
+        return link_img;
+    }
+
+    public void setLink_img(String link_img) {
+        this.link_img = link_img;
+    }
+}
