ade7d10984418ac47e4e257af60ce97aa1842bb8
mohammadhosseintutunchi <75937774+mohammadhosseintutunchi@users.noreply.github.com>
2021-08-09 11:57:12 +0430

    Shopping App

--- app/src/main/java/com/example/shoppingapp/adapter/SliderAdapter.java
+++ app/src/main/java/com/example/shoppingapp/adapter/SliderAdapter.java
@@ -1,4 +1,56 @@
 package com.example.shoppingapp.adapter;
 
-public class SliderAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.shoppingapp.R;
+import com.example.shoppingapp.model.Banner;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class SliderAdapter extends PagerAdapter {
+    Context context;
+    List<Banner> data;
+
+    public SliderAdapter(Context context, List<Banner> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.item_slider , container , false);
+        ImageView img_slider = view.findViewById(R.id.img_slider);
+
+        Picasso.get().load(data.get(position).getLink_img()).into(img_slider);
+
+        view.setRotationY(-180);
+        container.addView(view);
+        return view;
+    }
+
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
 }
--- app/src/main/java/com/example/shoppingapp/fragment/HomeFragment.java
+++ app/src/main/java/com/example/shoppingapp/fragment/HomeFragment.java
@@ -3,24 +3,45 @@ package com.example.shoppingapp.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.shoppingapp.R;
+import com.example.shoppingapp.adapter.SliderAdapter;
+import com.example.shoppingapp.model.Banner;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     public HomeFragment() {
     }
-View view;
+
+    View view;
+    //Slider
+    List<Banner> listBanner = new ArrayList<>();
+    SliderAdapter sliderAdapter;
+    ViewPager viewPager;
+    TabLayout tabs;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home, container, false);
+        viewPager = view.findViewById(R.id.viewpager);
+        tabs = view.findViewById(R.id.tabs);
         return view;
+    }
+    public static void test(){
+        int x;
+
+        
     }
 }
  
